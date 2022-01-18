007c360caaf41254abaaefb0fc949bc2b6e49269
blorenzo-ceibal <95303321+blorenzo-ceibal@users.noreply.github.com>
2022-01-18 14:35:41 -0300

    Definición de namespace y grupos

---
+++ par_impar.ts
@@ -0,0 +1,11 @@
+// Añade tu código aquí
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=190 weight=100 icon="\uf1ec" block="Personalizados"
+//% groups=['Matematica', 'LEDs', 'others']
+namespace par_impar {
+    
+
+
+}
